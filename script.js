e2d52bc19f222aaa8efdb5fb6c4d4f8310e208d4
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2024-11-03 19:59:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function ask(userQuestion) {
   setTimeout((function() {
     (async() => {
       const parsedUserQuestion = parseUserQuestion(userQuestion);
-      sendLog(userQuestion, String(parsedUserQuestion));
+      sendLog(userQuestion, JSON.stringify(parsedUserQuestion));
       if ((parsedUserQuestion.searchWord == '') || (parsedUserQuestion.needKey
           .length == 0)) {
         post('ごめんなさい！その質問はわからないや…');
