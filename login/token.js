55a2cf0cffc43da3438145ebd6ec7be36562ae71
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2024-11-04 18:10:01 +0900

    Update token.js

--- login/token.js
+++ login/token.js
@@ -26,7 +26,7 @@ if (!authCode) {
       const data = await response.json();
       const token = encodeURIComponent(JSON.stringify(data));
       
-      document.cookie = `authToken=${token}; max-age=120`;
+      document.cookie = `auth_token=${token}; max-age=120`;
       window.location.replace('/?authed=true');
     } catch (error) {
       window.location.replace('/error?status=' + encodeURIComponent(error.message));
