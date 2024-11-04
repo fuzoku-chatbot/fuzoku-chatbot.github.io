01a29c49f8c4331cd448c1f816b9caa009f4fda1
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2024-11-04 19:00:09 +0900

    Update auth.js

--- app/auth.js
+++ app/auth.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       }
     });
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      window.alert(`HTTP error! status: ${response.status}`);
     }
     userData = await response.json();
     window.alert(JSON.stringify(userData));
