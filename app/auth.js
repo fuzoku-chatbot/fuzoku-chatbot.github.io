969b117c1da1e123ec6cbc86bb152218916ef56e
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2024-11-04 21:09:50 +0900

    Update auth.js

--- app/auth.js
+++ app/auth.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
   let token = JSON.parse(decodeURIComponent(getCookieValue('auth_token')));
   if(!token) token = {access_token:'undefined'};
+  window.alert(token.access_token);
   
   if ((authed == 'true') && (token['access_token'] != 'undefined')) {
     let userData = '';
