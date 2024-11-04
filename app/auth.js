e8d064919181296257ce5be00694f022b878ffd5
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2024-11-04 17:19:34 +0900

    Update auth.js

--- app/auth.js
+++ app/auth.js
@@ -1,14 +1,14 @@
-let url = new URL(window.location.href);
-let authed = url.searchParams.get('authed');
+document.addEventListener('DOMContentLoaded', async () => {
+  let url = new URL(window.location.href);
+  let authed = url.searchParams.get('authed');
 
-function getCookieValue(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-  return null;
-}
+  function getCookieValue(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+  }
 
-(async () => {
   const token = JSON.parse(decodeURIComponent(getCookieValue('authToken')));
   if ((authed == 'true') && (token['access_token'])) {
     try {
@@ -21,10 +21,17 @@ function getCookieValue(name) {
       window.location.href = '/error?status=500';
     }
   } else {
-    try{
-    document.getElementById('authbackground').classList.remove('authed');
-    url.searchParams.remove('authed');
-    document.cookie = "authToken=; max-age=0";
-    }catch(error){window.alert(error.message)}
+    try {
+      const authBackground = document.getElementById('authbackground');
+      if (authBackground) {
+        authBackground.classList.remove('authed');
+      } else {
+        console.error("Element with ID 'authbackground' not found.");
+      }
+      url.searchParams.remove('authed');
+      document.cookie = "authToken=; max-age=0";
+    } catch (error) {
+      window.alert(error.message);
+    }
   }
-})();
+});
