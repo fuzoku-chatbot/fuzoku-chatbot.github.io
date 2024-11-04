38de6eabd31dd28b6d910d965b90ea6a0dd77303
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2024-11-04 16:21:26 +0900

    Update auth.js

--- app/auth.js
+++ app/auth.js
@@ -1,3 +1,19 @@
 let url = new URL(window.location.href);
-let authCode = url.searchParams.get('code');
-if (!authCode) 
+let authCode = url.searchParams.get('authed');
+
+function getCookieValue(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
+
+const token = JSON.parse(decodeURIComponent(getCookieValue('authToken')));
+if ((authed == 'true') && token['access_token']) {
+  fetch('/app/script.js').then(r=>r.text()).then(t=>eval(t)).then(()=>{ ... }
+  document.cookie = "authToken=; max-age=0";
+} else {
+  document.getElementsByClassName('auth').classList.remove('authed');
+  url.searchParams.remove('authed');
+  document.cookie = "authToken=; max-age=0";
+}
